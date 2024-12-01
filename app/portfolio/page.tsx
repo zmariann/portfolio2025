fea6d917676b5cf49a3625f77caad09fb1dc98c5
zmariann <zaszlosmariann@gmail.com>
2024-12-01 14:40:09 +0100

    design main page mobile view

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,8 +1,11 @@
 export default function Home() {
   return (
     <section className="flex items-center min-h-[60vh] justify-end">
-      <h1 className="text-right font-black text-5xl stroke m-10">
+      <h1 className="text-right font-black text-5xl lg:text-6xl strokeDesktop m-10 hidden sm:block">
         MULTIMEDIA CONTENT UNIQUELY DESIGNED FOR YOU.
+      </h1>
+      <h1 className="font-black text-4xl strokeMobile ml-10 sm:hidden block">
+        MULTIMEDIA CONTENT UNIQUELY DESIGNED <br></br> FOR YOU.
       </h1>
     </section>
   );
