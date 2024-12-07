88a7d7ecb70afcf3c91013a25fcce8f80f34d75d
zmariann <zaszlosmariann@gmail.com>
2024-12-07 16:07:40 +0100

    change a word in the intro text

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -2,10 +2,10 @@ export default function Home() {
   return (
     <section className="flex items-center min-h-[60vh] justify-end">
       <h1 className="text-right font-black text-5xl lg:text-6xl strokeDesktop m-10 hidden sm:block tracking-wider">
-        MULTIMEDIA CONTENT UNIQUELY DESIGNED FOR YOU.
+        AUDIOVISUAL CONTENT UNIQUELY DESIGNED FOR YOU.
       </h1>
       <h1 className="font-black text-4xl strokeMobile ml-10 sm:hidden block tracking-wide">
-        MULTIMEDIA CONTENT UNIQUELY DESIGNED <br></br> FOR YOU.
+        AUDIOVISUAL CONTENT UNIQUELY DESIGNED <br></br> FOR YOU.
       </h1>
     </section>
   );
