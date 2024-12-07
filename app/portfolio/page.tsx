ec6a76aa565ab2842c5f42d9797a9fd192bb49b1
zmariann <zaszlosmariann@gmail.com>
2024-12-07 20:45:48 +0100

    add react player

--- app/portfolio/page.tsx
+++ app/portfolio/page.tsx
@@ -1,12 +1,13 @@
+"use client";
+import IndexVideo from "@/app/_components/react-player";
+
 export default function Home() {
   return (
-    <section className="flex items-center min-h-[60vh] justify-end">
-      <h1 className="text-right font-black text-5xl lg:text-6xl strokeDesktop m-10 hidden sm:block tracking-wider">
+    <section className="flex sm:items-center min-h-[60vh] sm:justify-end flex-col lg:flex-row">
+      <h1 className="text-right font-black text-5xl strokeDesktop m-10 hidden sm:block tracking-wider">
         AUDIOVISUAL CONTENT UNIQUELY DESIGNED FOR YOU.
       </h1>
-      <h1 className="font-black text-4xl strokeMobile ml-10 sm:hidden block tracking-wide">
-        AUDIOVISUAL CONTENT UNIQUELY DESIGNED <br></br> FOR YOU.
-      </h1>
+      <IndexVideo></IndexVideo>
     </section>
   );
 }
