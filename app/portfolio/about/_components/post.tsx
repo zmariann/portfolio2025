7e068fd1af0d1d1e375163502c57c284cb518018
zmariann <zaszlosmariann@gmail.com>
2024-12-07 20:43:36 +0100

    final changes

--- app/portfolio/about/_components/post.tsx
+++ app/portfolio/about/_components/post.tsx
@@ -14,8 +14,11 @@ export default function Posts(props: Posts) {
 
   return (
     <div>
-      <button className="tracking-wider text-xl sm:text-2xl underline uppercase" onClick={() => setpopup(true)}>
-      {props.title}
+      <button
+        className="tracking-wider text-xl sm:text-2xl underline uppercase"
+        onClick={() => setpopup(true)}
+      >
+        {props.title}
       </button>
       <Popup trigger={popup} setTrigger={setpopup}>
         <div className="w-[90vw] h-[80vh] sm:w-[70vw] sm:h-[50vh] md:w-[80vw] md:h-[40vh] border-[1px] border-dark-shade  pl-5 py-3 pr-2 overflow-y-scroll scrollbar-hide overflow-scroll">
