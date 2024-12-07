0eaf38253a7c1d35e141596553914aa3e2abf433
zmariann <zaszlosmariann@gmail.com>
2024-12-07 17:40:34 +0100

    new mobile view design for -about- section

--- app/portfolio/about/page.tsx
+++ app/portfolio/about/page.tsx
@@ -8,35 +8,19 @@ import Posts from "./_components/post";
 
 export default function About() {
   return (
-    <>
-      <section className="sm:flex justify-center items-center min-h-[60vh] hidden mx-10">
-        <div>
-          <h1 className="text-right">
-            <Posts title={post1Title} textContent={post1Content}></Posts>
-          </h1>
-          <p className="tracking-wide text-2xl my-8 font-bold uppercase">
-            I help people bring their ideas to life with high-quality
-            audiovisual solutions.
-          </p>
-          <h1>
-            <Posts title={post2Title} textContent={post2Content}></Posts>
-          </h1>
-        </div>
-      </section>
-
-      <section className="min-h-[60vh] mx-5 sm:hidden">
-        <div className="mb-6">
-          <p className="tracking-wide font-black uppercase text-2xl mb-8">
-            I help people bring their ideas to life with high-quality
-            audiovisual solutions.
-          </p>
-          <div className="flex justify-center items-center flex-col gap-5">
-            <Posts title={post1Title} textContent={post1Content}></Posts>
-
-            <Posts title={post2Title} textContent={post2Content}></Posts>
-          </div>
-        </div>
-      </section>
-    </>
+    <section className="flex justify-center items-center min-h-[60vh] sm:mx-10 mx-6">
+      <div>
+        <h1 className="sm:text-right text-center">
+          <Posts title={post1Title} textContent={post1Content}></Posts>
+        </h1>
+        <p className="tracking-wide text-2xl my-3 font-bold uppercase">
+          I help people bring their ideas to life with high-quality audiovisual
+          solutions.
+        </p>
+        <h1 className="sm:text-left text-center">
+          <Posts title={post2Title} textContent={post2Content}></Posts>
+        </h1>
+      </div>
+    </section>
   );
 }
