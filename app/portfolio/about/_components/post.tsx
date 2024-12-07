baa64909961257eeda4ab0df5c2c486beec1a7ac
zmariann <zaszlosmariann@gmail.com>
2024-12-07 13:30:48 +0100

    about page mobile view

--- app/portfolio/about/_components/post.tsx
+++ app/portfolio/about/_components/post.tsx
@@ -14,14 +14,14 @@ export default function Posts(props: Posts) {
 
   return (
     <div>
-      <h1 className="font-black text-5xl strokeDesktop tracking-wide">
+      <h1 className="font-black sm:text-5xl sm:strokeDesktop strokeMobile text-4xl tracking-wide">
         {props.title}
       </h1>
-      <button className="underline" onClick={() => setpopup(true)}>
+      <button className="underline text-sm sm:text-base" onClick={() => setpopup(true)}>
         Read more...
       </button>
       <Popup trigger={popup} setTrigger={setpopup}>
-        <div className="w-[100vw] h-[90vh] sm:w-[70vw] sm:h-[50vh] md:w-[80vw] md:h-[40vh] border-[10px] border-mainBlack pl-5 py-3 pr-2 overflow-y-scroll scrollbar-hide overflow-scroll">
+        <div className="w-[100vw] h-[90vh] sm:w-[70vw] sm:h-[50vh] md:w-[80vw] md:h-[40vh] border-[1px] border-dark-shade pl-5 py-3 pr-2 overflow-y-scroll scrollbar-hide overflow-scroll">
           <Typewriter text={props.textContent} speed={20} />
         </div>
       </Popup>
--- app/portfolio/about/page.tsx
+++ app/portfolio/about/page.tsx
@@ -11,14 +11,31 @@ export default function About() {
     <>
       <section className="sm:flex justify-center items-center min-h-[60vh] hidden mx-10">
         <div>
-          <h1 className="flex justify-end text-right">
+          <h1 className="text-right">
             <Posts title={post1Title} textContent={post1Content}></Posts>
           </h1>
           <p className="tracking-wider text-2xl my-8">
             I help people bring their ideas to life with high-quality
             audiovisual solutions.
           </p>
-          <Posts title={post2Title} textContent={post2Content}></Posts>
+          <h1>
+            <Posts title={post2Title} textContent={post2Content}></Posts>
+          </h1>
+        </div>
+      </section>
+
+      <section className="min-h-[60vh] mx-10 sm:hidden">
+        <div className="mb-2">
+          <p className="tracking-wider text-xl mb-8">
+            I help people bring their ideas to life with high-quality
+            audiovisual solutions.
+          </p>
+          <h1 className="my-8">
+            <Posts title={post1Title} textContent={post1Content}></Posts>
+          </h1>
+          <h1>
+            <Posts title={post2Title} textContent={post2Content}></Posts>
+          </h1>
         </div>
       </section>
     </>
