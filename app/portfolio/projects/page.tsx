22ec09ce73ecda6500380eefc52412c219e4a095
zmariann <zaszlosmariann@gmail.com>
2024-12-07 20:50:51 +0100

    started projects page

--- app/portfolio/projects/page.tsx
+++ app/portfolio/projects/page.tsx
@@ -1,3 +1,21 @@
+import Link from "next/link";
+
+const projects = [
+  { name: "Videos", href: "/portfolio/projects/videos" },
+  { name: "Products", href: "/portfolio/projects" },
+  { name: "Coralized", href: "/portfolio/contact" },
+];
+
 export default function Projects() {
-  return <p>Online content can take many forms...</p>;
+  return (
+    <section className="flex justify-center items-center min-h-[60vh] flex-col sm:text-7xl text-5xl gap-4 font-[family-name:var(--regonia-font)]">
+      {projects.map((link) => {
+        return (
+          <Link key={link.name} href={link.href} className="">
+            <p> {link.name} </p>
+          </Link>
+        );
+      })}
+    </section>
+  );
 }
