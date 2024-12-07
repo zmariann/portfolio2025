3fe6284b236ed4a61e6c569d94b1faee36afd735
zmariann <zaszlosmariann@gmail.com>
2024-12-07 14:41:28 +0100

    remove unnecessary lines

--- app/portfolio/contact/page.tsx
+++ app/portfolio/contact/page.tsx
@@ -3,14 +3,12 @@ import { links } from "@/app/_components/links";
 export default function Contact() {
   return (
     <>
-      <section className="sm:flex justify-center items-center min-h-[60vh] hidden">
-        <p className="text-xl tracking-wider">CONTACT@ZMARIANN.COM</p>
-      </section>
-
-      <section className="flex justify-center items-center min-h-[60vh] sm:hidden">
+      <section className="flex justify-center items-center min-h-[60vh]">
         <div>
-          <p className="text-xl tracking-wider mx-1">CONTACT@ZMARIANN.COM</p>
-          <div className="tracking-wider flex justify-between mx-1">
+          <p className="text-xl tracking-wider mx-1 sm:mx-0">
+            CONTACT@ZMARIANN.COM
+          </p>
+          <div className="tracking-wider flex justify-between mx-1 sm:hidden">
             {links.map((link) => {
               return (
                 <a key={link.name} href={link.href}>
