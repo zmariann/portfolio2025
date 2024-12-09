18e48a8be0f4f1a2076265004a1ea412cbfb9b7a
zmariann <zaszlosmariann@gmail.com>
2024-12-09 19:59:34 +0100

    add back link to the videos page and change the design of it

--- app/portfolio/projects/videos/page.tsx
+++ app/portfolio/projects/videos/page.tsx
@@ -13,31 +13,35 @@ export default function Videos() {
     <>
       <section className="bg-red-400 flex justify-center items-center mt-auto">
         <div className="absolute mb-24">
-        <h1 className="relative text-7xl font-[family-name:var(--regonia-font)]">Videos</h1>
-        <div className="flex gap-6 ml-6 mt-2">
-          <div className="text-xl tracking-wider uppercase">
-            Music<br></br>Video
+          <h1 className="relative text-7xl font-[family-name:var(--regonia-font)]">
+            Videos
+          </h1>
+
+          <div className="flex gap-6 ml-6 mt-2">
+            <div className="text-xl tracking-wider uppercase">
+              Music<br></br>Video
+            </div>
+
+            <button
+              className="text-xl tracking-wider uppercase"
+              onClick={() => setpopup(true)}
+            >
+              TikTok/<br></br>Reels
+            </button>
+
+            <Popup trigger={popup} setTrigger={setpopup}>
+              <Gallery content={tiktokUrls}></Gallery>
+            </Popup>
+
+            <div className="text-xl tracking-wider uppercase">
+              Showreel/<br></br>2020
+            </div>
           </div>
-
-          <button
-            className="text-xl tracking-wider uppercase"
-            onClick={() => setpopup(true)}
-          >
-            TikTok/<br></br>Reels
-          </button>
-
-          <Popup trigger={popup} setTrigger={setpopup}>
-            <Gallery content={tiktokUrls}></Gallery>
-          </Popup>
-
-          <div className="text-xl tracking-wider uppercase">
-            Showreel/<br></br>2020
+          <div className="ml-6 mt-2">
+            <BackLink></BackLink>
           </div>
-        </div>
         </div>
       </section>
-      <div className="sm:hidden block"><BackLink></BackLink></div>
-      
     </>
   );
 }
