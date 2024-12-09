0ad92cc23cdb0253668bc9a5097c101ce7feb2f2
zmariann <zaszlosmariann@gmail.com>
2024-12-09 19:20:53 +0100

    videos page desktop view

--- app/portfolio/projects/videos/page.tsx
+++ app/portfolio/projects/videos/page.tsx
@@ -11,29 +11,33 @@ export default function Videos() {
 
   return (
     <>
-      <section>
-        <h1>Videos</h1>
-        <ul>
-          <li>
+      <section className="bg-red-400 flex justify-center items-center mt-auto">
+        <div className="absolute mb-24">
+        <h1 className="relative text-7xl font-[family-name:var(--regonia-font)]">Videos</h1>
+        <div className="flex gap-6 ml-6 mt-2">
+          <div className="text-xl tracking-wider uppercase">
             Music<br></br>Video
-          </li>
-          <li>
-            <button
-              className="tracking-wider text-xl sm:text-2xl underline uppercase"
-              onClick={() => setpopup(true)}
-            >
-              TikTok/<br></br>Reels
-            </button>
-            <Popup trigger={popup} setTrigger={setpopup}>
-              <Gallery content={tiktokUrls}></Gallery>
-            </Popup>
-          </li>
-          <li>
+          </div>
+
+          <button
+            className="text-xl tracking-wider uppercase"
+            onClick={() => setpopup(true)}
+          >
+            TikTok/<br></br>Reels
+          </button>
+
+          <Popup trigger={popup} setTrigger={setpopup}>
+            <Gallery content={tiktokUrls}></Gallery>
+          </Popup>
+
+          <div className="text-xl tracking-wider uppercase">
             Showreel/<br></br>2020
-          </li>
-        </ul>
+          </div>
+        </div>
+        </div>
       </section>
-      <BackLink></BackLink>
+      <div className="sm:hidden block"><BackLink></BackLink></div>
+      
     </>
   );
 }
