4748805bdd09976a57b64f31fb2050b01baeb7f5
zmariann <zaszlosmariann@gmail.com>
2024-12-11 16:08:13 +0100

    solve a runtime error

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.m?js/,
+      resolve: {
+        fullySpecified: false,
+      },
+    });
+    return config;
+  },
 };
 
 export default nextConfig;
