65228d575d2dcade4c38719277db4270223214dc
zmariann <zaszlosmariann@gmail.com>
2024-12-14 16:12:53 +0100

    use separate file for menus

---
+++ app/portfolio/projects/videos/_components/videos-menu.ts
@@ -0,0 +1,37 @@
+export const videosmenu = [
+  {
+    title1: "music",
+    title2: "video",
+    popupName: "musicvideo",
+    role1: "editor",
+    role2: "",
+  },
+  {
+    title1: "tiktok/",
+    title2: "reels",
+    popupName: "tiktok",
+    role1: "videographer",
+    role2: "post-production",
+  },
+  {
+    title1: "showreel",
+    title2: "2021",
+    popupName: "showreel2021",
+    role1: "videographer",
+    role2: "post-production",
+  },
+  {
+    title1: "gardening",
+    title2: "services",
+    popupName: "gardening",
+    role1: "post-production",
+    role2: "",
+  },
+  {
+    title1: "transvanilla",
+    title2: "1%",
+    popupName: "transvanilla",
+    role1: "videographer",
+    role2: "editor",
+  },
+];
