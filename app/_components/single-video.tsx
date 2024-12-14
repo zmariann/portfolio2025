78e7ba7c173919af940ddb6d15cab5afd355b9c0
zmariann <zaszlosmariann@gmail.com>
2024-12-14 16:07:07 +0100

    horizontal single video component

---
+++ app/_components/single-video.tsx
@@ -0,0 +1,23 @@
+"use client";
+import React from "react";
+import VideoPlayer from "./react-player";
+
+type VideoGalleryProps = {
+  content: string;
+};
+
+const SingleVideo: React.FC<VideoGalleryProps> = ({ content }) => {
+  return (
+    <div className="w-full sm:h-[90vh] h-screen">
+    <VideoPlayer
+      url={content}
+      controls={true}
+      muted={true}
+      playing={true}
+      loop={true}
+    />
+    </div>
+  );
+};
+
+export default SingleVideo;
