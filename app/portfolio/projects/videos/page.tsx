af4cbc46841a935bacf477ab90cdc5acbc474821
zmariann <zaszlosmariann@gmail.com>
2024-12-14 16:03:40 +0100

    remove duplications & set up popups

--- app/portfolio/projects/videos/page.tsx
+++ app/portfolio/projects/videos/page.tsx
@@ -4,57 +4,47 @@ import Popup from "@/app/_components/popup";
 import VideoGallery from "@/app/_components/videogallery";
 import { tiktokUrls } from "./_components/tiktok-videos";
 import BackLink from "../../../_components/backLink";
+import SingleVideo from "@/app/_components/singleVideo";
+import { videosmenu } from "./_components/videos-menu";
 
 export default function Videos() {
-  const [popup, setpopup] = useState(false);
+  const [activePopup, setActivePopup] = useState<null | string>(null);
+
+  const togglePopup = (popupKey: string | null) => {
+    setActivePopup(popupKey);
+  };
 
   return (
     <>
-      <section className="bg-red-400 sm:flex justify-center items-center mt-auto hidden">
+      <section className="md:flex justify-center items-center mt-auto hidden">
         <div className="absolute mb-24 mt-10">
           <h1 className="relative text-7xl font-[family-name:var(--regonia-font)]">
             Videos
           </h1>
 
           <div className="flex gap-6 ml-6 mt-2">
-            
-            <div className="flex justify-start flex-col">
-              <button className="text-left text-xl tracking-wider uppercase"
-              onClick={() => setpopup(true)}>
-                Music<br></br>Video
-              </button>
-              <p className="flex flex-col text-sm lowercase">editor</p>
-            </div>
-
-            <div className="flex justify-start flex-col">
-              <button
-                className="text-left text-xl tracking-wider uppercase"
-                onClick={() => setpopup(true)}
-              >
-                TikTok/<br></br>Reels
-              </button>
-              <p className="flex flex-col text-sm lowercase">
-                videographer<br></br>post-production
-              </p>
-            </div>
-
-            <div className="flex justify-start flex-col">
-              <button className="text-left text-xl tracking-wider uppercase">
-                Showreel<br></br>2021
-              </button>
-              <p className="flex flex-col text-sm lowercase">
-                videographer <br></br>post-production
-              </p>
-            </div>
-
-            <div className="flex justify-start flex-col">
-              <button className="text-left text-xl tracking-wider uppercase">
-                Transvanilla <br></br> 1%
-              </button>
-              <p className="flex flex-col text-sm lowercase">
-                videographer<br></br>editor
-              </p>
-            </div>
+            {videosmenu.map((project) => {
+              return (
+                <div
+                  key={project.popupName}
+                  className="flex justify-start flex-col"
+                >
+                  <button
+                    className="text-left text-xl tracking-wider uppercase"
+                    onClick={() => togglePopup(project.popupName)}
+                  >
+                    {project.title1}
+                    <br></br>
+                    {project.title2}
+                  </button>
+                  <p className="flex flex-col text-sm lowercase">
+                    {project.role1}
+                    <br></br>
+                    {project.role2}
+                  </p>
+                </div>
+              );
+            })}
           </div>
           <div className="ml-6 mt-2">
             <BackLink></BackLink>
@@ -62,53 +52,58 @@ export default function Videos() {
         </div>
       </section>
 
-      <section className="flex justify-center items-center flex-col w-full min-h-[60vh] sm:hidden mb-5">
+      <section className="flex justify-center items-center flex-col w-full min-h-[60vh] md:hidden mb-5">
         <div className="flex flex-col items-start gap-3 m-3">
           <h1 className="flex flex-col text-5xl font-[family-name:var(--regonia-font)]">
             Videos
           </h1>
 
-          <div>
-            <button className="text-lg tracking-wider uppercase text-left">
-              Music Video
-            </button>
-            <p className="flex flex-col text-sm">editor</p>
-          </div>
-
-          <div>
-            <button
-              className="text-lg tracking-wider uppercase text-left"
-              onClick={() => setpopup(true)}
-            >
-              TikTok / Reels
-            </button>
-            <p className="flex flex-col text-sm">
-              videographer | post-production
-            </p>
-          </div>
-
-          <div>
-            <button className="text-lg tracking-wider uppercase text-left">
-              Showreel 2021
-            </button>
-            <p className="flex flex-col text-sm">
-              videographer | post-production
-            </p>
-          </div>
-
-          <div>
-            <div className="text-lg tracking-wider uppercase">
-              Transvanilla 1%
-            </div>
-            <p className="flex flex-col text-sm"> videographer | editor</p>
-          </div>
+          {videosmenu.map((project) => {
+            return (
+              <div key={project.popupName}>
+                <button
+                  onClick={() => togglePopup(project.popupName)}
+                  className="text-lg tracking-wider uppercase text-left"
+                >
+                  {project.title1} {project.title2}
+                </button>
+                {project.role2 === "" ? (
+                  <p className="flex flex-col text-sm">{project.role1}</p>
+                ) : (
+                  <p className="flex flex-col text-sm">
+                    {project.role1} | {project.role2}
+                  </p>
+                )}
+              </div>
+            );
+          })}
           <div>
             <BackLink></BackLink>
           </div>
         </div>
       </section>
-      <Popup trigger={popup} setTrigger={setpopup}>
-        <VideoGallery content={tiktokUrls}></VideoGallery>
+
+      <Popup
+        trigger={activePopup !== null}
+        setTrigger={() => setActivePopup(null)}
+      >
+        {activePopup === "musicvideo" && (
+          <SingleVideo content="https://www.youtube.com/watch?v=AFMhjvr_Uj4" />
+        )}
+
+        {activePopup === "tiktok" && <VideoGallery content={tiktokUrls} />}
+
+        {activePopup === "showreel2021" && (
+          <SingleVideo content="/showreel2021.mp4" />
+        )}
+
+        {activePopup === "transvanilla" && (
+          <SingleVideo content="https://www.youtube.com/watch?v=A5Zav6wC-AE" />
+        )}
+
+        {activePopup === "gardening" && (
+          <SingleVideo content="/furgeFugeBemutatkozo.mp4" />
+        )}
       </Popup>
     </>
   );
