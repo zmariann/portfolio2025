2c7a6e601b050cfd4b195d3fd0c4a2e446c67f97
zmariann <zaszlosmariann@gmail.com>
2024-12-14 19:47:46 +0100

    projects & photography set up for first ver to upload to storage

--- app/portfolio/projects/page.tsx
+++ app/portfolio/projects/page.tsx
@@ -2,20 +2,21 @@ import Link from "next/link";
 
 const projects = [
   { name: "Videos", href: "/portfolio/projects/videos" },
-  { name: "Products", href: "/portfolio/projects" },
-  { name: "Coralized", href: "/portfolio/contact" },
+  { name: "Photography", href: "/portfolio/projects/photography" },
 ];
 
 export default function Projects() {
   return (
-    <section className="flex justify-center items-center min-h-[60vh] flex-col sm:text-7xl text-5xl gap-4 font-[family-name:var(--regonia-font)]">
-      {projects.map((link) => {
-        return (
-          <Link key={link.name} href={link.href} className="">
-            <p> {link.name} </p>
-          </Link>
-        );
-      })}
+    <section className="flex justify-center items-center min-h-[60vh] flex-col sm:text-7xl text-[15vw] gap-4 font-[family-name:var(--regonia-font)]">
+      <div>
+        {projects.map((link) => {
+          return (
+            <Link key={link.name} href={link.href}>
+              <p> {link.name} </p>
+            </Link>
+          );
+        })}
+      </div>
     </section>
   );
 }
---
+++ app/portfolio/projects/photography/page.tsx
@@ -0,0 +1,39 @@
+import BackLink from "@/app/_components/backLink";
+
+export default function Photography() {
+  return (
+    <section className="w-full min-h-[60vh] flex justify-center items-center flex-col">
+      <div className="m-5">
+        <h1>| Under construction |</h1>
+        <p className="mb-6">
+          For more info get in touch: CONTACT@ZMARIANN.COM{" "}
+        </p>
+        <div className="mb-6">
+          <p className="mb-2">
+            Check my product photography portfolio{" "}
+            <a
+              href="/photography/product.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline uppercase"
+            >
+              here.
+            </a>
+          </p>
+          <p>
+            Check my event photography portfolio{" "}
+            <a
+              href="/photography/event.pdf"
+              target="_blank"
+              rel="noopener noreferrer"
+              className="underline uppercase"
+            >
+              here.
+            </a>
+          </p>
+        </div>
+        <BackLink></BackLink>
+      </div>
+    </section>
+  );
+}
