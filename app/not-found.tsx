df031a26ed019bc6c175bf3af68ab25be94142c3
zmariann <zaszlosmariann@gmail.com>
2024-12-17 19:27:05 +0100

    not found page for errors

---
+++ app/not-found.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <main className="w-full h-screen flex justify-center items-center flex-col">
+      <p className="uppercase">/ Page not found /</p>
+      <Link className="underline" href="/portfolio">Click here to go back</Link>
+    </main>
+  );
+}
