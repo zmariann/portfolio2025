28992a9f7c4ba686fa24b547b437a393075c1c1a
zmariann <zaszlosmariann@gmail.com>
2024-12-18 17:03:37 +0100

    design change

--- app/_components/single-video.tsx
+++ app/_components/single-video.tsx
@@ -9,14 +9,15 @@ type VideoGalleryProps = {
 const SingleVideo: React.FC<VideoGalleryProps> = ({ content }) => {
   return (
     <div className="w-full flex justify-center items-center sm:h-screen mb-10 h-screen">
-      <div className="sm:h-[80vh] sm:w-[90%] w-full h-[85vh]">    <VideoPlayer
-      url={content}
-      controls={true}
-      muted={false}
-      playing={true}
-      loop={true}
-    /></div>
-
+      <div className="sm:h-[80vh] sm:w-[90%] w-full h-[85vh]">
+        <VideoPlayer
+          url={content}
+          controls={true}
+          muted={false}
+          playing={true}
+          loop={true}
+        />
+      </div>
     </div>
   );
 };
